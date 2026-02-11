dcc6cb608916ff5414e79490fda3de9291353a08
oc-odin-turner-25 <odin.turner@student.onslow.school.nz>
2026-02-11 22:12:42 +0000

    Working on function parameters

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,66 +1,21 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-func showWelcome() {
-    print("Welcome")
-    
+
+func roomVolume() -> Double {
+    return 3 * 2 * 1 
 }
 
-func printTitle() {
-    print("Room Calculator")
-}
-
-func printDivider() {
-    print("------------------")
-    
-}
-
-
-
-func readNumber(prompt: String) -> Double {
-    print(prompt)
-    var returnValue  = -1.0
-    guard let input = readLine(), let number = Double(input), number > 0 else{
-        print("Invalid number")
-        return readNumber(prompt: prompt)
-    }
-    returnValue = number
-    return returnValue 
+func printArea(width: Double, length: Double) {
+    print("\(width * length)m²")
 }
 
 
 @main
 struct SwiftPlayground {
-
-
     static func main() {  
-        let maximumItemVolume = 2.0 
+    print("\(roomVolume())m³")
+    printArea(width: 3.0, length: 2.5)
 
-        let roomLength = readNumber(prompt: "Enter the room Length:")
-        let roomWidth = readNumber(prompt: "Enter the room Width:")
-        let roomHeight = readNumber(prompt: "Enter the room Height:")
 
-        let roomArea = roomLength * roomWidth
-        let roomVolume = roomArea * roomHeight
-        
-        print("Room area: \(roomArea)m²") 
-        print("Room volume: \(roomVolume)m³")
-        
-        let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-
-        var totalFurnitureVolume = 0.0
-
-        furnitureVolumes.enumerated().forEach { index, volume in 
-            print("item\(index + 1): \(volume) m³")
-            if volume > maximumItemVolume {
-                print("Oversized item detected ")
-            }
-            totalFurnitureVolume = totalFurnitureVolume + volume
-        
-
-        let usableVolume = volume - totalFurnitureVolume
-        print("usable volume: \(usableVolume)m³")
-
-        }
     }
-    }
-
+}   
