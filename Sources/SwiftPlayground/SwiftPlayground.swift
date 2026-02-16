7f309c12742a985624731807cd4cdd3d9235b294
oc-odin-turner-25 <odin.turner@student.onslow.school.nz>
2026-02-16 23:27:04 +0000

    Functions and parameters completed

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,13 +9,26 @@ func printArea(width: Double, length: Double) {
     print("\(width * length)m²")
 }
 
+func area(width: Double, length: Double) -> Double {
+    return width * length
+}
+
+func volume(width: Double, length: Double, height: Double) -> Double {
+    return width * length * height 
+}
+
+
 
 @main
 struct SwiftPlayground {
-    static func main() {  
+    static func main() {
+
     print("\(roomVolume())m³")
     printArea(width: 3.0, length: 2.5)
-
-
+    let area = area(width: 2.7, length: 4.2)
+    print("\(area)m²")
+    let volume = volume(width: 3.5, length: 2.5, height: 1.46)
+    print("\(volume)m³")
+    
     }
 }   
