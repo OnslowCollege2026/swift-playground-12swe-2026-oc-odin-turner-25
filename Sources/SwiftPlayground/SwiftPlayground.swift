5a1533bebf304f0a37d52926753983bd70b6ab15
oc-odin-turner-25 <odin.turner@student.onslow.school.nz>
2026-02-23 23:06:52 +0000

    add egg worked on

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,33 +1,89 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func roomVolume() -> Double {
-    return 3 * 2 * 1 
+
+
+/// Prints the main manu 
+/// - Returns: user menu selction
+func showMenu() -> Int {
+    print ("""
+    =====Egg Shop=====
+        1. Add eggs
+        2. Sell eggs 
+        3. Show current stock 
+        4. Show sales total 
+        5. Exit 
+    Choose an option:
+    """)
+    
+    guard let userInput = readLine() ,  let userInput = Int(userInput) , 
+    userInput > 0 , userInput < 6
+    else {
+        print("Please choose one of the 5 options.")
+        return 6
+    } 
+    return userInput
 }
 
-func printArea(width: Double, length: Double) {
-    print("\(width * length)m²")
+
+/// asks user how many eggs are being added
+/// Limits: > 1, < 1,000
+/// - Parameter eggsInStock: The cuurent number of eggs in stock 
+func addEggs(eggsInStock: Int) {
+    print("Enter number of eggs being added to the stock:")
+    guard let addedEggs = readLine(), let addedEggs = Int(addedEggs) , 
+addedEggs < 0 , addedEggs + eggsInStock < 1000
+else {
+    return
+}
 }
 
-func area(width: Double, length: Double) -> Double {
-    return width * length
+
+func eggStock(eggsInStock: Int) {
+    print("""
+    -----Current Egg Stock-----
+    eggs: \(eggsInStock)
+    """)
 }
 
-func volume(width: Double, length: Double, height: Double) -> Double {
-    return width * length * height 
+
+/// calls a function based on users input
+/// - Parameter currentMenuChoice: Users input from the main menu 
+func menuChoice(currentMenuChoice: Int) {
+if currentMenuChoice == 1 {
+    addEggs(eggsInStock: )
 }
+else if currentMenuChoice == 2  {
+    
+}
+else if currentMenuChoice == 3 {
+    
+    
+}
+else if currentMenuChoice == 4  {
+    
+}
+else if currentMenuChoice == 5 {
+    
+}
+else if currentMenuChoice == 6 {
+    
+}
+}
+
 
 
 @main
 struct SwiftPlayground {
     static func main() {
+        
+        var ProgramIsRunning = true
+        var eggsSold = 0
+        var currentEggStock = 20 
 
-    print("\(roomVolume())m³")
-    printArea(width: 3.0, length: 2.5)
-    let area = area(width: 2.7, length: 4.2)
-    print("\(area)m²")
-    let volume = volume(width: 3.5, length: 2.5, height: 1.46)
-    print("\(volume)m³")
+        while ProgramIsRunning == true {}
+        let menuInput = showMenu()
+        menuChoice(currentMenuChoice: menuInput)
     
     }
-}   
+}
