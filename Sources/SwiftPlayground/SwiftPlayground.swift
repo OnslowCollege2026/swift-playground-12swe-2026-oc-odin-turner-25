2357f1150d4282064e431b84bb8aa528d95d635c
oc-odin-turner-25 <odin.turner@student.onslow.school.nz>
2026-04-24 03:06:10 +0000

    24/4 work

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+// Give user a list of options to choose from.
 func Menu(){
     print("""
     ------Kumura Stall------
@@ -12,8 +13,32 @@ func Menu(){
     """)
 }
 
-func PurchaseKumura(){
+func PurchaseKumura(kumuraStock: Double,) -> Double {
 
+    var funcRunning = true
+    while funcRunning == true {
+        print("""
+        There are currently \(kumuraStock)Kgs of kumura in stock
+        how many Kumura(kg) would you like to purchase?
+        (each bag holds 5Kg)
+        (kumura costs $30/kg)
+        """)
+
+        if let input = readLine(), let sale = Double(input), sale <= kumuraStock, sale >= 0.1, sale <= 5 {
+
+            let bagsUsed = sale/5
+
+            print("""
+            You have \(sale)Kgs of kumura split between 
+            
+            """)
+            
+            return kumuraStock
+        }
+        else{
+
+        }
+    }
 }
 
 
@@ -22,10 +47,11 @@ func PurchaseKumura(){
 struct SwiftPlayground {
     static func main() {
     
-        /// Current nuber of kumura in stock
+        /// Current number of kumura in stock.
         var kumuraStock = 10.00
         /// Max number of kumura that ccan be stored. (Kg)
         let storageMax = 50.00 
+        /// Number of menu options avalable.
         let menuOptions = 5
 
 
@@ -37,7 +63,6 @@ struct SwiftPlayground {
             if let input = readLine(), let userChoice = Int(input), userChoice <= menuOptions, userChoice >= 1{}
             else {
                 print("Invalid option please try again.")
-            
             }
 
 
