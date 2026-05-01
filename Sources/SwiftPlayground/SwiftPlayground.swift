943a621797217a3e04878c9823a715b3ca73a382
oc-odin-turner-25 <odin.turner@student.onslow.school.nz>
2026-05-01 03:08:23 +0000

    1/5

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-// Give user a list of options to choose from.
+/// Give user a list of options to choose from.
 func Menu(){
     print("""
     ------Kumura Stall------
@@ -15,6 +15,10 @@ func Menu(){
 }
 
 
+/// asks how many kumura the user is purchasing and also how bags they want.
+/// 
+/// Returns the amount of kumura purchaseed as a Double.
+/// Also returns the amount of bags purchased as an Int.
 func PurchaseKumura(kumuraStock: Double,) -> (kumuraChange: Double, bagsChange: Int,) {
 
     while true {
@@ -58,6 +62,9 @@ func PurchaseKumura(kumuraStock: Double,) -> (kumuraChange: Double, bagsChange: 
     }
 
 
+/// Tells user current amount of kumura in storage amd the maximum storage space
+/// 
+/// returns the Number of kumura added 
 func addStock(kumuraStock: Double, storageMax: Double) -> Double{
     while true {
     print("""
@@ -77,17 +84,64 @@ func addStock(kumuraStock: Double, storageMax: Double) -> Double{
     }
 }
 
+
+/// Prints the current number of kumura in stock 
+func viewCurrentStock(kumuraStock: Double) {
+    print("""
+    ==== Current Stock ====
+    kumura: \(kumuraStock)Kgs 
+
+
+    """)
+    
+}
+
+func salesRecord(KumuraSales: [Double], bagSales: [Int]) {
+    var totalKumuraSales = 0.00
+    KumuraSales.forEach({ kumura in  
+    totalKumuraSales += kumura })
+
+    var totalBagSales = 0
+    bagSales.forEach({ bags in  
+    totalBagSales += bags })
+
+    print("""
+    ==== Sales Record ====
+    kumura: \(totalKumuraSales)Kgs
+    Bags: \(totalBagSales)
+
+
+    """)
+    
+}
+
+func infoSummary(KumuraSales: [Double], bagSales: [Int]) {
+
+var adverageWeight = 0.00
+var totalBagSales = 0
+    KumuraSales.forEach({ kumura in  
+    adverageWeight = kumura * bagSales.forEach({ bags in  
+    totalBagSales += bags 
+    }) 
+    })
+
+}
 @main
 struct SwiftPlayground {
     static func main() {
     
         /// Current number of kumura in stock.
-        var kumuraStock = 10.00
+        var stock = 10.00
         /// Max number of kumura that ccan be stored. (Kg)
         let storageMax = 50.00 
         /// Number of menu options avalable.
         let menuOptions = 6
 
+        var bagsSale: [Int] = []
+
+        var kumuraSale: [Double] = []
+
+        
 
 
         var programRunning = true
@@ -98,12 +152,26 @@ struct SwiftPlayground {
             if let input = readLine(), let userChoice = Int(input), userChoice <= menuOptions, userChoice >= 1{
 
                 if userChoice == 1 {
-                    let purchaseFunc = PurchaseKumura(kumuraStock: kumuraStock)
+                    let purchaseFunc = PurchaseKumura(kumuraStock: stock)
+                    bagsSale.append(purchaseFunc.bagsChange) 
+                    kumuraSale.append(purchaseFunc.kumuraChange)
+                    stock -= purchaseFunc.kumuraChange
+
                 }
                 
                 else if userChoice == 2{
-                    let addStockFunc = addStock(kumuraStock: kumuraStock, storageMax: storageMax)
+                    let addStockFunc = addStock(kumuraStock: stock, storageMax: storageMax)
+                    stock += addStockFunc
                 }
+
+                else if userChoice == 3 {
+                    viewCurrentStock(kumuraStock: stock)
+                }
+
+                else if userChoice == 4 {
+                    salesRecord(KumuraSales: kumuraSale, bagSales: bagsSale)
+                }
+
             }
             else {
                 print("Invalid option please try again.")
@@ -111,7 +179,7 @@ struct SwiftPlayground {
         
         
 
-        kumuraStock -= purchasefunc.kumuraChange
+        
         }
     }
 }
